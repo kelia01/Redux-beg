66d95081270708ebe88f15d5112ce9979aa2cbcc
Kelia Iradukunda <iradukundakelia@gmail.com>
2025-08-31 22:44:13 +0200

    Login change name to mine

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,7 +6,7 @@ function Login() {
 
   return (
     <div>
-        <button onClick={() => {dispatch(login({ name: "Pedro", age: 20 }))}}>Login</button>
+        <button onClick={() => {dispatch(login({ name: "Kelia", age: 20 }))}}>Login</button>
         <button onClick={() => {dispatch(logout({ name: "Pedro", age: 20 }))}}>Logout</button>
     </div>
   )
